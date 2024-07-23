e93c2190b3b3b526128e8be40facd230ad4e3943
J Lynch <jlynch908@g.rwu.edu>
2024-07-22 21:10:36 -0400

    Initial commit.

---
+++ Scripts/Global/char_relationships.gd
@@ -0,0 +1,15 @@
+extends Node
+
+# set up as "character_name = int"
+# keeps track of the player's relationships with the other characters
+
+# bandagram players
+var dropout : int = 0
+var nerd : int = 0
+var lawyer : int = 0
+var cop : int = 0
+var janitor : int = 0
+
+# non-players
+var detective : int = 0
+
---
+++ Scripts/Global/char_status.gd
@@ -0,0 +1,14 @@
+extends Node
+
+# set up as "character_name = boolean"
+# if true, that character is alive
+
+# bandagram players
+var dropout : bool = true
+var nerd : bool = true
+var lawyer : bool = true
+var cop : bool = true
+var janitor : bool = true
+
+# non-players
+var detective : bool = true
